eac4982b35452a09f0e26f75c8b7747c76cbd7c0
Don <dagem.alemseged1@gmail.com>
2022-11-08 03:07:51 -0500

    Basic calculator functions work

--- js/script.js
+++ js/script.js
@@ -1,13 +1,71 @@
-const displayNow = document.querySelector('#display-current')
 const displayBefore = document.querySelector('#display-last')
-const clear = document.querySelector('.clear')
-// const addition = document.querySelector('.add')
-// const subtraction = document.querySelector('.sub')
-// const multiplication = document.querySelector('.mult')
-// const division = document.querySelector('.div')
-const operators = document.querySelectorAll('.btn-op')
+const displayNow = document.querySelector('#display-current')
 const compute = document.querySelector('.equals')
+const clear = document.querySelector('.clear')
 const numbers = document.querySelectorAll('.btn-num')
+const operators = document.querySelectorAll('.btn-op')
+
+let firstOperand = ''
+let secondOperand = ''
+let operator = ''
+let resetDisplay = false
+
+numbers.forEach(btn => {
+    btn.addEventListener('click', () => {
+        appendInput(btn.textContent)
+    })
+})
+
+operators.forEach(btn => {
+    btn.addEventListener('click', () => {
+        appendOperation(btn.textContent)
+        check = true
+    })
+})
+
+compute.addEventListener('click', () => {
+    if (displayNow.textContent !== '' && operator !== '') {
+        secondOperand = Number(displayNow.textContent)
+        displayBefore.textContent = `${firstOperand} ${operator} ${secondOperand} =`
+        firstOperand = operate(firstOperand, operator, secondOperand);
+        displayNow.textContent = firstOperand;
+    }
+})
+
+clear.addEventListener('click', () => {
+    displayNow.textContent = '0'
+    displayBefore.textContent = ''
+    firstOperand = ''
+    secondOperand = ''
+    operator = ''
+    check = false
+})
+
+let appendInput = num => {
+    if (displayNow.textContent === '0' || check === true) {
+        displayNow.textContent = num
+        check = false
+    }
+    else 
+        displayNow.textContent += num
+}
+
+let appendOperation = op => {
+    if (displayBefore.textContent === '') {
+        firstOperand = Number(displayNow.textContent)
+        operator = op
+        displayBefore.textContent = `${firstOperand} ${operator}`
+        displayNow.textContent = `${firstOperand}`
+    }
+    else {
+        secondOperand = Number(displayNow.textContent)
+        // secondExists = true
+        operator = op
+        displayBefore.textContent = `${firstOperand} ${operator} ${secondOperand} =`
+        firstOperand = operate(firstOperand, operator, secondOperand)
+        displayNow.textContent = `${firstOperand}`
+    }
+}
 
 let add = (a, b) => {
     return a + b
@@ -22,47 +80,16 @@ let multiply = (a, b) => {
 }
 
 let divide = (a, b) => {
-    if (b === '0') {
-        alert ("You can't divide by zero!")
-        return;
-    }
     return a / b
 }
 
 let operate = (a, operation, b) => {
     if (operation === '+') 
-        console.log(add(a, b))
+        return add(a, b)
     else if (operation === '-')
-        console.log(subtract(a, b))
-    else if (operation === '*')
-        console.log(multiply(a, b))
-    else if (operation === '/')
-        console.log(divide(a, b))
-}
-
-clear.addEventListener('click', () => {
-    displayNow.textContent = '0'
-    displayBefore.textContent = ''
-})
-
-numbers.forEach(btn => {
-    btn.addEventListener('click', () => {
-        appendInput(btn.textContent)
-    })
-})
-
-operators.forEach(btn => {
-    btn.addEventListener('click', num => {
-        if (displayBefore.textContent === '') {
-            displayBefore.textContent +=  displayNow.textContent
-            displayBefore.textContent += btn.textContent
-        }
-    })
-})
-
-let appendInput = num => {
-    if (displayNow.textContent === '0')
-        displayNow.textContent = num
-    else 
-        displayNow.textContent += num
+        return subtract(a, b)
+    else if (operation === 'x')
+        return multiply(a, b)
+    else if (operation === '%')
+        return divide(a, b)
 }
